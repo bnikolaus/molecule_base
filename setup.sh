134dcb5a05fe59345d214f6f40a08c1554828852
Brent Nikolaus <bnikolaus.contractor@somos.com>
2018-04-13 13:26:41 -0700

    fixing library pulls

--- setup.sh
+++ setup.sh
@@ -25,62 +25,98 @@ color_test () {
 }
 
 clean() {
-echo -e $RED"Removing all requirements"
-cd $molecule_path; rm -rf \
-bin include lib pip-selfcheck.json test
+
+  echo -e $RED"Removing all requirements"
+  home_path
+  rm -rf \
+  bin include lib pip-selfcheck.json test
+
 }
 
 make() {
-echo -e $BLUE"Creating Virtualenv"
-virtualenv --python=python2.7 $molecule_path
-source ./bin/activate
-mkdir roles
+
+  echo -e $BLUE"Creating Virtualenv"
+  virtualenv --python=python2.7 $molecule_path
+  source ./bin/activate
+  mkdir roles
+
 }
 
 requirements() {
-echo -e $BLUE"Installing requirements for molecule"
-pip2.7 install --trusted-host pypi.python.org -r requirements.txt
+
+  echo -e $BLUE"Installing requirements for molecule"
+  pip2.7 install --trusted-host pypi.python.org -r requirements.txt
+
 }
 
 init() {
-if [ -d "$role_path/base" ]; then
-  echo -e $BLUE"skipping initilization of molecule role base"
-else
-  echo -e $YELLOW"Creating molecule base" 
-  cd $role_path 
-  ../bin/molecule init template \
-  --url https://github.com/bnikolaus/cookiecutter-molecule \
-  --role-name base --no-input
+
+  if [ -d "$role_path/base" ]; then
+    echo -e $BLUE"skipping initilization of molecule role base"
+  else
+    echo -e $YELLOW"Creating molecule base" 
+    role_path 
+    ../bin/molecule init template \
+    --url https://github.com/bnikolaus/cookiecutter-molecule \
+    --role-name base --no-input
 fi
 }
 
 link() {
-if [ -L "base" ]; then
-  echo -e $BLUE"skipping link creation" 
-else
-  echo -e $YELLOW"Creating links to base"
-  pwd
-  cd $molecule_path
-  ln -s ./roles/base test
-fi
+  
+  if [ -L "base" ]; then
+    echo -e $BLUE"skipping link creation" 
+  else
+    echo -e $YELLOW"Creating links to base"
+    pwd
+    cd $molecule_path
+    ln -s ./roles/base test
+  fi
+
 }
 
 library() {
 
-cd $molecule_path
-input="library.yml"
-while IFS= read -r var
-do
-  repo_url="$(cut -d'|' -f2 <<<"$var")"   
-  repo_name="$(cut -d'|' -f1 <<<"$var")"
-  repo_hash="$(cut -d'|' -f3 <<<"$var")"
-  echo -e $BLUE"downloading $repo_name $repo_hash" 
-  cd $role_path
-  git clone $repo_url $repo_name 
-done < "$input"
+  home_path
+  input="library.yml"
+  while IFS= read -r var
+  do
+    repo_url="$(cut -d'|' -f2 <<<"$var")"   
+    repo_name="$(cut -d'|' -f1 <<<"$var")"
+    repo_hash="$(cut -d'|' -f3 <<<"$var")"
+    echo $repo_url
+    echo $repo_name
+    echo $repo_hash
+    role_path
+    if [ -d $repo_name ]; then
+      echo -e $GREEN"skipping git clone"
+      cd $repo_name
+      echo -e $YELLOW"Checking out $repo_hash"
+      git checkout $repo_hash
+    else 
+      echo -e $GREEN"downloading $repo_name $repo_hash" 
+      git clone $repo_url $repo_name
+    fi
+  done < "$input"
 
 }
 
+
+home_path() {
+
+  cd $molecule_path
+
+}
+
+role_path() {
+
+  cd $role_path
+
+}
+
+
+#role_path
+#home_path
 #color_test
 #clean
 #make
